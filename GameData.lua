2fc39a5cb931e93692bccf1063fc7a9385416e2c
Mr-LWG <114276968+Mr-LWG@users.noreply.github.com>
2025-07-04 18:04:34 +0800

    Update GameData.lua

--- GameData.lua
+++ GameData.lua
@@ -7,19 +7,14 @@ local UpdateContent={
 	{ 
 		Type="table",
 		Content={
-			{
-				{ "NoveSkills",  {				--更新整个NoveSkills表
-						[1] = {ID = 1, Reward = "133023", TotalRaffles = 3, },
-						[2] = {ID = 2, Reward = "133024", TotalRaffles = 6, },
-						[3] = {ID = 3, Reward = "133025", TotalRaffles = 40, },
-					},
-				},
-				
-				{ "NoveSkills", 1,  {ID = 1, Reward = "133023", TotalRaffles = 4, } }, 	--更新NoveSkills表中索引为1的内容
-					
-	
-
+			{ "NoveSkills",  {				--更新整个NoveSkills表
+				[1] = {ID = 1, Reward = "133023", TotalRaffles = 50, },
+				[2] = {ID = 2, Reward = "133024", TotalRaffles = 10, },
+				[3] = {ID = 3, Reward = "133025", TotalRaffles = 40, },
 			},
+			},
+
+			{ "NoveSkills", 1,  {ID = 1, Reward = "133023", TotalRaffles = 4, } }, 	--更新NoveSkills表中索引为1的内容
 		},
 	},
 }
@@ -32,7 +27,7 @@ local UpdateContent={
 	{
 		Type="table",
 		Content={
-			
+
 			{"RedeemCode","629023", {ID="629023",Note="MOD福利码",Code="E5QTNIV222",Enable=1,UseOnce=1,Rewards="100001,5000",Deadline="20260101",Release=0,}, }
 		}
 	},
